a29c7c4e536ee49a2e5845f14e688e1612ea126e
Catalina Hernandez <cataxto@gmail.com>
2023-04-25 18:38:35 -0500

    Parametros en campos Django

--- proyDjango1/applications/departamento/admin.py
+++ proyDjango1/applications/departamento/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import departamentoDB
 
 # Register your models here.
+
+
+admin.site.register(departamentoDB)
--- proyDjango1/applications/departamento/models.py
+++ proyDjango1/applications/departamento/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class departamentoDB(models.Model):
+    name = models.CharField('Nombre', max_length=50, blank=True) #editable=False -> no podran editarlo desde el navegador
+    short_name = models.CharField('Nombre Corto', max_length=20, unique=True, null=True)
+    anulate = models.BooleanField('Anulado',default=False)
+    fecha = models.DateField()
+
+    def __str__(self):
+        return str(self.id)+'-'+self.name+'-'+self.short_name+'-'+str(self.fecha) +'-'+str(self.anulate)
