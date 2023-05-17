7a0d64b441d029aeb268e07a08dc1a8cc696d573
cataxto <36272762+cataxto@users.noreply.github.com>
2023-05-16 22:06:41 -0500

    Delete personas_django directory

--- personas_django/sap/webapp/views.py
+++
@@ -1,9 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-
-# Create your views here.
-
-def bienvenido(request): #request contiene informacion de la peticion que se envia desde el navegador al serv django
-    return HttpResponse('Hola mundo desde Django')
-def despedirse(request):
-    return HttpResponse('Adios')
